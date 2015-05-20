bd251631b06303624297d98723bdcd815c77ab57
Sylwia M. Bialczak <argoncode18@gmail.com>
2015-05-20 10:48:26 -0700

    Comment out JIRA related tests

--- lib/factor-connector-jira.rb
+++ lib/factor-connector-jira.rb
@@ -21,7 +21,7 @@ class JiraConnectorDefinition < Factor::Connector::Definition
   end
 
   resource :jira_all do
-    action :connect do |params|
+    action :connect do |params| # just for testing for now
       client = init_jira(params)
     end
   end
@@ -51,64 +51,8 @@ class JiraConnectorDefinition < Factor::Connector::Definition
 
     create_new_issue = client.Issue.build
     create_new_issue.save(issue_fields)
-    binding.pry
-
+    # what to return ?
     end
   end
 
 end
-
-  #
-  # resource :client do
-  #   action :all do |params|
-  #     chef = init_chef(params)
-  #     respond safe('Fetching all clients') {|x| chef.clients.all }
-  #   end
-  #
-  #   action :get do |params|
-  #     chef = init_chef(params)
-  #     id   = params.varify(:id, required:true)
-  #     respond safe("Getting client with id '#{id}'"){ chef.clients.fetch(id) }
-  #   end
-  #
-  #   action :create do |params|
-  #     chef = init_chef(params)
-  #     name = params.varify(:name, is_a:String, required:true)
-  #
-  #     available_params = %w(admin public_key private_key validator)
-  #     client_params = {name:name}.marge(pull_params(params,available_params))
-  #
-  #     respond safe("Getting client with id '#{id}'"){ chef.clients.create(client_params) }
-  #   end
-  #
-  #   action :delete do |params|
-  #     chef = init_chef(params)
-  #     id   = params.varify(:id, required:true)
-  #     respond safe("Deleting client with id '#{id}'"){ chef.clients.fetch(id).destroy }
-  #   end
-  # end
-
-  # resource :projects do
-  #
-  #   # factor call =>> 'jira::projects::list'
-  #   action :list do |params|
-  #     username = params.varify(:username,required:true,is_a:String)
-  #     password = params.varify(:password,required:true,is_a:String)
-  #     site     = params.varify(:site, required:true, is_a:String)
-  #
-  #     info "Connecting to '#{site}' as '#{username}'"
-  #     client   = JIRA::Client.new(username:username, password:password, auth_type: :basic, site:site)
-  #
-  #     respond []
-  #   end
-  # end
-# end
-
-
-
-# Show all projectsjira_calls_data
-# projects = client.Project.all
-
-# projects.each do |project|
-#   puts "Project -> key: #{project.key}, name: #{project.name}, id: #{project.id}"
-# end
--- spec/jira_spec.rb
+++ spec/jira_spec.rb
@@ -5,22 +5,22 @@ describe JiraConnectorDefinition do
   # describe 'projects' do
   #   describe 'list' do
   #     it 'can get a list' do
-  #       # @runtime.start
   #       @runtime.run([:projects,:list], username:@username, password:@password, site:@site)
-  #       # binding.pry
-  #       expect(@runtime).to respond
+  #       a = []
+  #       expect(@runtime.logs).to eq a
+  #       expect(@runtime).to message info:"Registering web hook"
   #     end
   #   end
   # end
   #
-  describe 'issues' do
-    describe 'add' do
-      it 'creates new issue on the web' do
-        @runtime.run([:issues,:add], username:@username, password:@password, site:@site)
-        expect(@runtime).to respond
-      end
-    end
-  end
+  # describe 'issues' do
+  #   describe 'add' do
+  #     it 'creates new issue on the web' do
+  #       @runtime.run([:issues,:add], username:@username, password:@password, site:@site)
+  #       expect(@runtime).to respond
+  #     end
+  #   end
+  # end
   #
   # describe 'jira_all' do
   #   describe 'connect' do
